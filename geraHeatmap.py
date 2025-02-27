d46a538f310b4c0a44c40c8c1e73f3b15ff38db7
Pedro Nadotti <nadottipedro5@gmail.com>
2025-02-27 18:51:45 -0300

    Adicionando requirements.txt

--- geraHeatmap.py
+++ geraHeatmap.py
@@ -1,3 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
+df = pd.read_excel('DataForTable2.1.xls')
+
+print(df.head())
+
+corr = df.corr(numeric_only=True)
+print(corr)
+
+
+plt.figure(figsize=(12, 10))
+map = sns.heatmap(corr, annot=True, cmap='coolwarm', fmt='.2f', robust=True)
+plt.title('Heatmap do Nadottins')
+plt.show()
--- geraScatter.py
+++ geraScatter.py
@@ -1,3 +1,15 @@
-import pandas
-import matplotlib.pyplot as plt
-import altair
+import pandas as pd
+import altair as alt
+
+df = pd.read_excel('NomeDoArquivo.xls')
+
+charte = alt.Chart(df).mark_point(filled=True, size=100).encode(
+    x= 'Eixo X',
+    y= 'Eixo Y',
+    color='Cores',
+    tooltip=['Informacoes ao passar o mouse por cima']
+).properties(
+    width=800,
+    height=800).interactive()
+
+charte.save('scatter.html')
